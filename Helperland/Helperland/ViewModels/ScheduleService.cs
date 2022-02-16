298f6c8e6bda39f4d64bb5e39e0ee4a7f0bcc6fa
Tanmay Mathur <60750240+Tanmay-Mathur@users.noreply.github.com>
2022-02-16 18:08:36 +0530

    Add files via upload

---
+++ Helperland/Helperland/ViewModels/ScheduleService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Helperland.ViewModels
+{
+    public class ScheduleService
+    {
+        [Required]
+        public DateTime Date{ get; set; }
+        [Required]
+        public string Time { get; set; }
+
+        [Required]
+        public int Duration { get; set; }
+
+        [Required]
+        public Extra extra { get; set; }
+
+        public String Comments { get; set; }
+
+        public bool havePet { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public bool Remember { get; set; }
+    }
+}
