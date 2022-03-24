9611bdd7adb18e8e72bad1e22c06c038aaf82129
Tanmay Mathur <60750240+Tanmay-Mathur@users.noreply.github.com>
2022-03-24 23:59:28 +0530

    Add files via upload

---
+++ Helperland/Helperland/ViewModels/AdminUserMng.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Helperland.ViewModels
+{
+    public class AdminUserMng
+    {
+
+        public int? UserId { get; set; }
+        public string UserName { get; set; }
+
+        public string UserType { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public string City { get; set; }
+
+        public string PhoneNo { get; set; }
+
+        public string RegDate { get; set; }
+
+        public bool? Status { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime? FromDate { get; set; }
+
+        public DateTime? ToDate { get; set; }
+
+    }
+}
