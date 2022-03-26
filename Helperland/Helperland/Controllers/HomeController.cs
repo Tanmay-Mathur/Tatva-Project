6623afe8ac2f138af35531d388c3a9dd259b14ac
Tanmay Mathur <60750240+Tanmay-Mathur@users.noreply.github.com>
2022-03-26 20:13:48 +0530

    Add files via upload

---
+++ Helperland/Helperland/Controllers/HomeController.cs
@@ -0,0 +1,208 @@
+﻿using Helperland.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Helperland.Data;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using System.Net.Mail;
+
+namespace Helperland.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly HelperlandContext _db;
+
+        private readonly IWebHostEnvironment _webHostEnv;
+
+        
+
+        /*   public HomeController(ILogger<HomeController> logger)
+           {
+               _logger = logger;
+           }
+   */
+        public HomeController(HelperlandContext db, IWebHostEnvironment webHostEnv)
+        {
+            _db = db;
+            _webHostEnv = webHostEnv;
+        }
+
+        public IActionResult Index()
+        {
+            
+            int? id = HttpContext.Session.GetInt32("userId");
+            if (id == null && Request.Cookies["userid"] != null)
+            {
+                HttpContext.Session.SetInt32("userId", Convert.ToInt32(Request.Cookies["userId"]));
+                id = HttpContext.Session.GetInt32("userId");
+            }
+            
+            if (id != null)
+            {
+                var user = _db.Users.FirstOrDefault(x => x.UserId == id);
+                TempData["name"] = user.FirstName;
+                TempData["userType"] = user.UserTypeId.ToString();
+            }
+            
+            return PartialView();
+        }
+
+        public IActionResult About()
+        {
+            int? id = HttpContext.Session.GetInt32("userId");
+            if (id == null && Request.Cookies["userid"] != null)
+            {
+                HttpContext.Session.SetInt32("userId", Convert.ToInt32(Request.Cookies["userId"]));
+                id = HttpContext.Session.GetInt32("userId");
+            }
+
+            if (id != null)
+            {
+                var user = _db.Users.FirstOrDefault(x => x.UserId == id);
+                TempData["name"] = user.FirstName;
+                TempData["userType"] = user.UserTypeId.ToString();
+            }
+            
+            return PartialView();
+        }
+
+        public IActionResult Contact()
+        {
+            int? id = HttpContext.Session.GetInt32("userId");
+            if (id == null && Request.Cookies["userid"] != null)
+            {
+                HttpContext.Session.SetInt32("userId", Convert.ToInt32(Request.Cookies["userId"]));
+                id = HttpContext.Session.GetInt32("userId");
+            }
+
+            if (id != null)
+            {
+                var user = _db.Users.FirstOrDefault(x => x.UserId == id);
+                TempData["name"] = user.FirstName;
+                TempData["userType"] = user.UserTypeId.ToString();
+            }
+            
+            return PartialView();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactU contactu)
+        {
+
+            if (ModelState.IsValid)
+            {
+                string serverFolder = "";
+                if (contactu.Attach != null)
+                {
+                    string folder = "contactFiles/";
+                    folder += Guid.NewGuid().ToString() + "_" + contactu.Attach.FileName;
+                    serverFolder = Path.Combine(_webHostEnv.WebRootPath, folder);
+
+                    FileStream files = new FileStream(serverFolder, FileMode.Create);
+                    contactu.Attach.CopyToAsync(files);
+                    contactu.FileName = folder;
+                    files.Close();
+                }
+                contactu.CreatedOn = DateTime.Now;
+                var contactData = _db.ContactUs.Add(contactu);
+                _db.SaveChanges();
+
+                List<string> emails = _db.Users.Where(x => x.UserTypeId == 3).Select(x => x.Email).ToList();
+
+                string msg = "<p> user name :- " + contactu.FirstName + " " + contactu.LastName + " </p>" + 
+                    "<p>mobile number :- " + contactu.PhoneNumber + " </p>" +"<p>email :- " + contactu.Email + "</p>" + "<p>query type :- " + contactu.Subject + "</p>" + "<p> message :- " + contactu.Message + "</p>" + "createdBy :- " + contactu.CreatedBy + "</p><p> createOn :- " + contactu.CreatedOn + "</p>";
+
+                SendContactMail(msg, emails, serverFolder);
+                return RedirectToAction("Index", "Home", new { msgSent = "true" });
+            }
+            return PartialView();
+
+        }
+        private static void SendContactMail(string msg, List<string> emails, string path)
+        {
+            SmtpClient client = new SmtpClient("smtp.gmail.com");
+            client.Port = 587;
+            client.UseDefaultCredentials = true;
+            client.EnableSsl = true;
+            client.Credentials = new System.Net.NetworkCredential("tanmaymathur1927@gmail.com", "tanmay@123");
+
+            MailMessage message = new MailMessage();
+            
+                message.Subject = "New Message from Helperland";
+                message.Body = msg;
+            
+            if(path != "")
+            {
+                Attachment attach = new Attachment(path);
+                message.Attachments.Add(attach);
+            }
+
+            message.From = new MailAddress("tanmaymathur1927@gmail.com");
+            message.IsBodyHtml = true;
+            foreach (string email in emails)
+            {
+                message.To.Add(email);
+            }
+            client.Send(message);
+        }
+
+
+        public IActionResult Prices()
+        {
+            int? id = HttpContext.Session.GetInt32("userId");
+            if (id == null && Request.Cookies["userid"] != null)
+            {
+                HttpContext.Session.SetInt32("userId", Convert.ToInt32(Request.Cookies["userId"]));
+                id = HttpContext.Session.GetInt32("userId");
+            } 
+
+            if (id != null)
+            {
+                var user = _db.Users.FirstOrDefault(x => x.UserId == id);
+                TempData["name"] = user.FirstName;
+                TempData["userType"] = user.UserTypeId.ToString();
+            }
+            
+            return PartialView();
+        }
+
+        public IActionResult Faq()
+        {
+            int? id = HttpContext.Session.GetInt32("userId");
+            if (id == null && Request.Cookies["userid"] != null)
+            {
+                HttpContext.Session.SetInt32("userId", Convert.ToInt32(Request.Cookies["userId"]));
+                id = HttpContext.Session.GetInt32("userId");
+            }
+
+            if (id != null)
+            {
+                var user = _db.Users.FirstOrDefault(x => x.UserId == id);
+                TempData["name"] = user.FirstName;
+                TempData["userType"] = user.UserTypeId.ToString();
+            }
+            
+            return PartialView();
+        }
+
+       
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Helperland/Helperland/Controllers/SignupController.cs
@@ -0,0 +1,221 @@
+﻿using Helperland.Data;
+using Helperland.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+
+namespace Helperland.Controllers
+{
+    public class SignupController : Controller
+    {
+        private readonly HelperlandContext _db;
+
+        public SignupController(HelperlandContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Signup()
+        {
+            int? Id = HttpContext.Session.GetInt32("userId");
+            
+            if(Id != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return PartialView();
+        }
+
+        [HttpPost]
+        public IActionResult Signup(User user)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if (_db.Users.Where(x => x.Email == user.Email).Count() == 0 && _db.Users.Where(x => x.Mobile == user.Mobile).Count() == 0)
+                {
+                    user.UserTypeId = 1;
+                    user.CreatedDate = DateTime.Now;
+                    user.ModifiedDate = DateTime.Now;
+                    user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
+                    _db.Users.Add(user);
+                    _db.SaveChanges();
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ViewBag.Message = "User already exists";
+                    return PartialView();
+                }
+
+            }
+            return PartialView();
+        }
+
+        public IActionResult BecomeHelper()
+        {
+            return PartialView();
+        }
+
+   
+        [HttpPost]
+        public IActionResult BecomeHelper(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_db.Users.Where(x => x.Email == user.Email).Count() == 0 && _db.Users.Where(x => x.Mobile == user.Mobile).Count() == 0)
+                {
+                    user.UserTypeId = 2;
+                    user.CreatedDate = DateTime.Now;
+                    user.ModifiedDate = DateTime.Now;
+                    user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
+                    _db.Users.Add(user);
+                    _db.SaveChanges();
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ViewBag.Message = "User already exists";
+                    return PartialView();
+                }
+
+            }
+            return PartialView();
+
+        }
+
+
+        [HttpPost]
+        public IActionResult Login(LoginUser loginUser)
+        {
+            if (ModelState.IsValid)
+            {
+                User userLogin = _db.Users.FirstOrDefault(x => x.Email == loginUser.Email);
+                if(userLogin != null)
+                {
+
+                    string password = userLogin.Password;
+                
+                    bool pass = BCrypt.Net.BCrypt.Verify(loginUser.Password, password);
+                    if (_db.Users.Where(x => x.Email == loginUser.Email && pass).Count() > 0)
+                    {
+                        var user = _db.Users.FirstOrDefault(x => x.Email == loginUser.Email);
+
+                        if (loginUser.Remember == true)
+                        {
+                            CookieOptions cookieRemember = new CookieOptions();
+                            cookieRemember.Expires = DateTime.Now.AddMinutes(60);
+                            Response.Cookies.Append("userId", Convert.ToString(user.UserId), cookieRemember);
+
+                        }
+
+                        HttpContext.Session.SetInt32("userId", user.UserId);
+
+                        if (user.UserTypeId == 1)
+                        {
+                            return RedirectToAction("CustServiceHistory", "CustomerPage");
+                        }
+                        else if (user.UserTypeId == 2)
+                        {
+                            return RedirectToAction("SPUpcomingService", "ServicePro");
+                        }
+                        else if (user.UserTypeId == 3)
+                        {
+                            return RedirectToAction("ServiceRequest", "Admin");
+                        }
+                    }   
+                }
+               
+                    TempData["showAlert"] = "show alert";
+                    TempData["loginFail"] = "Username and Password are invalid.";
+                    return RedirectToAction("Index", "Home", new { loginFail = "true" });
+                
+            }
+            
+            return PartialView();
+        }
+
+        
+        [HttpPost]
+        public IActionResult SendMail(string email)
+        {
+
+            if (email != null)
+            {
+                var user = _db.Users.FirstOrDefault(x => x.Email == email);
+
+                SmtpClient client = new SmtpClient("smtp.gmail.com");
+                client.Port = 587;
+                client.UseDefaultCredentials = true;
+                client.EnableSsl = true;
+                client.Credentials = new System.Net.NetworkCredential("Id", "Pwd");
+
+                MailMessage message = new MailMessage();
+                
+                
+                    message.Subject = "Reset Password";
+                    message.Body = "<h1>Reset your password by click below link</h1>" +
+                    "<a href='" + Url.Action("ResetPassword", "Signup", new { userId = user.UserId, token = user.Password }, "https") + "'>Reset Password</a>";
+          
+                message.From = new MailAddress("tanmaymathur1927@gmail.com");
+                message.IsBodyHtml = true;
+
+                message.To.Add(user.Email);
+                client.Send(message);
+
+
+                
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult ResetPassword(int userId, string token)
+        {
+            TempData["id"] = userId;
+            User user = _db.Users.FirstOrDefault(x => x.UserId == userId);
+            if(token == user.Password)
+            {
+                return PartialView();
+            }
+            return RedirectToAction("Index", "Home");
+        }
+      
+        [HttpPost]
+        public IActionResult ResetPassword(ResetPassword rp)
+        {
+            String HashPwd = BCrypt.Net.BCrypt.HashPassword(rp.password);
+            var user = new User() { UserId = rp.userId, Password = HashPwd, ModifiedDate = DateTime.Now };
+            _db.Users.Attach(user);
+            _db.Entry(user).Property(x => x.Password).IsModified = true;
+            _db.Entry(user).Property(x => x.ModifiedDate).IsModified = true;
+            _db.SaveChanges();
+
+
+            return RedirectToAction("Index", "Home", new { loginModal = "true" });
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+
+            foreach(var cookie in Request.Cookies.Keys)
+            {
+                Response.Cookies.Delete(cookie);
+            }
+            
+            
+            return RedirectToAction("Index", "Home", new { logoutModal = "true"});
+        }
+
+        
+    }
+
+
+
+}
